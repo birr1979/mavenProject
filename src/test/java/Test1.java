833d80c412c27179b52006b3db831ddb07b25a87
birr1979 <bag1979mp>
2021-07-12 00:06:20 -0400

    1st commit by B

---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args) {
+        String y="Birhanu dagnew";
+        int count;
+      count=y.split(" ").length;
+        System.out.println(count);
+
+       }
+}
