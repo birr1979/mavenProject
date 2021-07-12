8bca2a746c50d42cd63c7328ad4be54e38c8abce
birrpro <68886608+birr1979@users.noreply.github.com>
2021-07-12 00:11:57 -0400

    Update Test1.java
    adding the method

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -6,4 +6,16 @@ public class Test1 {
         System.out.println(count);
 
        }
+    
+    //new method to read the max number from the array
+    int array={12,3,4,5,6,1,0,3};
+    //using the for loop
+    int max=array[0];
+    
+    for(int i=0;i<array.size();i++){
+        if(array[i]>max){
+            max=array[i]
+            }
+    }
+    system.out.println(max+ ": is the maximum number in the given array.)
 }
